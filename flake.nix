32b5bf57905eac0f17d1c1955421c050a018fcb5
Jez Cope <j.cope@erambler.co.uk>
2023-09-12 12:19:52 +0100

    get working nix environment

--- flake.nix
+++ flake.nix
@@ -1,31 +1,33 @@
 {
   description = "Description for the project";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    mach-nix.url = "github:DavHau/mach-nix";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
-  outputs = inputs@{ nixpkgs, flake-parts, mach-nix, ... }:
+  outputs = inputs@{ flake-parts, mach-nix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
+      imports = [ ];
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          python = mach-nix.lib.${system}.mkPython {
+            requirements = builtins.readFile ./requirements.txt;
+            providers = {
+              webcolors = "sdist";
+              jsonschema = "sdist";
+            };
+          };
+        in {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
 
-      ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-
-        devShells.default = mach-nix.lib.${system}.mkPythonShell {
-          requirements = builtins.readFile ./requirements.txt;
+          packages = {
+            inherit python;
+            default = python;
+          };
+          devShells.default = python.env;
         };
-      };
       flake = {
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
