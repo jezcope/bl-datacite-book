a56e387c45322f2ee6f9df2ba1cb1ad8b847f307
Jez Cope <j.cope@erambler.co.uk>
2023-11-17 11:15:15 +0000

    switch to poetry2nix for better reproducibility

--- flake.nix
+++ flake.nix
@@ -2,30 +2,23 @@
   description =
     "A set of documented examples of tasks that can be performed using the DataCite API";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
 
-  outputs = inputs@{ flake-parts, mach-nix, ... }:
+  outputs = inputs@{ flake-parts, nixpkgs, poetry2nix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ ];
       systems =
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
         let
-          mach-nix' = mach-nix.lib.${system};
-          python = mach-nix'.mkPython {
-            requirements = (builtins.readFile ./requirements.txt) + "pip";
-            providers = {
-              webcolors = "sdist";
-              jsonschema = "sdist";
-            };
+          inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv;
+          myPython = mkPoetryEnv {
+            projectDir = ./.;
+            preferWheels = true;
           };
-        in {
-          packages = {
-            inherit python;
-            default = python;
-          };
-
-          devShells.default = python.env;
-        };
+        in { devShells.default = myPython.env; };
     };
 }
